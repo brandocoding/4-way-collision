e1b4fb13bf3a5d7dacd7a95bf9d3387eb6a17a58
brandocoding <41130732+brandocoding@users.noreply.github.com>
2022-03-12 19:21:35 -0600

    Add files via upload

---
+++ 4 - Way Collision/main.py
@@ -0,0 +1,82 @@
+import pygame as pg
+import random
+from settings import *
+from sprites import *
+
+class Game:
+    def __init__(self):
+        # initialize game window, etc
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.running = True
+
+    def new(self):
+        # start a new game
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.player = Player(self)
+        self.plt = Platform(WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20)
+        self.plt_2 = Platform(0, HEIGHT - 40, WIDTH, 40)
+        self.all_sprites.add(self.plt)
+        self.all_sprites.add(self.plt_2)
+        self.platforms.add(self.plt)
+        self.platforms.add(self.plt_2)
+        self.all_sprites.add(self.player)
+        self.run()
+
+    def run(self):
+        # Game Loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        # Game Loop - Update
+        self.all_sprites.update()
+        # check if player hits a platform - only if falling
+
+        hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+        if hits:
+            self.player.pos.y = hits[0].rect.top 
+            self.player.vel.y = 0
+
+    def events(self):
+        # Game Loop - events
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
+
+    def draw(self):
+        # Game Loop - draw
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        # *after* drawing everything, flip the display
+        pg.display.flip()
+
+    def show_start_screen(self):
+        # game splash/start screen
+        pass
+
+    def show_go_screen(self):
+        # game over/continue
+        pass
+
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_go_screen()
+
+pg.quit()
---
+++ 4 - Way Collision/settings.py
@@ -0,0 +1,16 @@
+# game options/settings 
+TITLE = "Jumpy!"
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.8
+
+# define colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
